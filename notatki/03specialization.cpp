ac7f2ab4d39fab9644b720671baa2fab9c252033
Anna Kwadrans <akwadran@c3r14s2.42madrid.com>
2026-07-05 17:17:30 +0200

    spec notes

---
+++ notatki/03specialization.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "03specialization.tpp"
+
+int main(void)
+{
+    Pair<int, int>              p1(4, 2);
+    Pair<std::string, float>    p2(std::string("Pi"), 3.14f);
+    Pair<float, bool>           p3(4.2f, true);
+    Pair<bool, bool>            p4(true, false);
+
+    std::cout << p1 << std::endl
+        << p2 << std::endl
+        << p3 << std::endl
+        << p4 << std::endl;
+
+    return (0);
+}
