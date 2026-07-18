d0bcdfa5f6409fe1d778239984eaf9d58c63bdec
Anna Kwadrans <annakwadrans@MacBook-Air-de-Anna.local>
2026-07-18 15:54:42 +0200

    iter final

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,18 +1,15 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
-#include <iostream>
+# include <iostream>
+# include <string.h>
 
-template<typename T>
-void    iter(T *arr, int const len, void (*func)(T const &))
-{
-    for (int i = 0; i < len; i++)
-        func(arr[i]);
-    return ;
-}
+//template<typename T>
+//void    iter(T *arr, int len, void (*func)(T &))
+//void    iter(T *arr, int len, void (*func)(T const &))
 
-template<typename T>
-void    iter(T *arr, int const len, void (*func)(T &))
+template<typename T, typename F>
+void    iter(T *arr, int len, F func)
 {
     for (int i = 0; i < len; i++)
         func(arr[i]);
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,6 +1,6 @@
 #include "iter.hpp"
 
-void    print(int &n) {
+void    printInt(int const n) {
     std::cout << n << std::endl;
     return;
 }
@@ -10,14 +10,33 @@ void    increase(int &n) {
     return ;
 }
 
+void    printStr(std::string const & str) {
+    std::cout << str << std::endl;
+    return;
+}
+
+void    toUpper(std::string &str) {
+    for (size_t i = 0; i < str.length(); i++)
+        str[i] = std::toupper(str[i]);
+}
+
 int main(void)
 {
 
     int integers[4] = {1, 2, 3, 4};
 
-    iter<int>(integers, 4, print);
     iter<int>(integers, 4, increase);
-    iter<int>(integers, 4, print);
+    iter<int>(integers, 4, printInt);
+    std::cout << std::endl;
+    
+    int const   cIntegers[4] = {10, 9, 8, 7};
+    iter<int const>(cIntegers, 4, printInt);
+    std::cout << std::endl;
+
+    std::string strings[4] = {"Hola", "mundo", "42", "string!"};
+
+    iter<std::string>(strings, 4, toUpper);
+    iter<std::string const>(strings, 4, printStr);
 
     return (0);
 }
